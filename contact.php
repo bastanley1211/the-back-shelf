1450c5031db0a66845f57d2e70008bafad404278
bastanley1211 <77221444+bastanley1211@users.noreply.github.com>
2021-01-29 20:01:40 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,5 @@
+<html>
+  <body>
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
@@ -8,3 +10,5 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
+  </body>
+</html>
